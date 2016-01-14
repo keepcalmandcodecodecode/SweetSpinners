4aad95438beab1004d606f775708a62d6b28fa2b
keepcalmandcodecodecode <easy.dev@yandex.ru>
2016-01-14 23:47:25 +0300

    Initial

---
+++ Example/Pods/Target Support Files/Pods-SweetSpinners_Example/Pods-SweetSpinners_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SweetSpinners_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SweetSpinners_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/SweetSpinners/SweetSpinners-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SweetSpinners : NSObject
+@end
+@implementation PodsDummy_SweetSpinners
+@end
